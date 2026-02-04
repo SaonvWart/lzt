6992c4ed7473bb9cb5398f4a1a3763f6f579fa0e
SaonvWart <2055640882p@gmail.com>
2026-02-04 22:13:04 +0800

    Update SITE_TITLE to include emoji

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "極樂堂|樂子新傳媒";
+export const SITE_TITLE = "🤣極樂堂";
 export const SITE_DESCRIPTION = "極樂堂新傳媒，持續創作免費的樂子快報";
