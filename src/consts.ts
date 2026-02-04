373ed46537b0ea2eacc4a2a5f9accaaf39ebc612
SaonvWart <2055640882p@gmail.com>
2026-02-04 20:56:37 +0800

    Update site title and description to Chinese

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "極樂堂|樂子新傳媒";
+export const SITE_DESCRIPTION = "極樂堂新傳媒，持續創作免費的樂子快報";
